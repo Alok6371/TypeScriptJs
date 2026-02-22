3a4000de467a298db731a2f6cac6a07ea84e71ec
Alok pradhan <palokpradhan6371@gmail.com>
2026-02-22 15:09:59 +0530

    update

--- 02_Project/src/Componets/SwipeForPayment.tsx
+++ 02_Project/src/Componets/SwipeForPayment.tsx
@@ -3,52 +3,43 @@ import { motion, useMotionValue } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 
 const SwipePayment: React.FC = () => {
-
-  const navigate = useNavigate();
+  //   const navigate = useNavigate();
 
   const x = useMotionValue(0);
 
-  const handleDragEnd = () => {
+  const navigate = useNavigate();
 
-    if (x.get() > 220) {
+  const handleDrag = () => {
+    const swipePosition = x.get();
 
-      alert("Booking Confirmed ✅");
-
-      navigate("/payment");
-
+    if (swipePosition < 250) {
+      x.set(0);
+    } else {
+      setTimeout(() => {
+        navigate("/payment");
+      }, 2000);
     }
   };
 
   return (
-
-    <div className="flex justify-center items-center h-screen">
-
+    <div>
       <div className="w-80 h-16 bg-gray-200 rounded-full relative shadow-lg overflow-hidden">
-
-        <p className="absolute inset-0 flex items-center justify-center font-semibold">
-          Swipe To Confirm →
+        <p className="absolute inset-0 flex items-center justify-center font-semibold text-lg">
+          Swipe to Book →
         </p>
-
         <motion.div
-
           drag="x"
-
-          dragConstraints={{ left: 0, right: 240 }}
-
           style={{ x }}
-
-          onDragEnd={handleDragEnd}
-
-          className="w-14 h-14 bg-green-500 rounded-full absolute top-1 left-1 cursor-pointer flex items-center justify-center text-white"
-
+          dragConstraints={{ left: 0, right: 255 }}
+          onDragEnd={handleDrag}
+          dragElastic={0}
+          dragMomentum={false}
+          className="w-14 h-14 rounded-full bg-red-500 absolute top-1 left-1 cursor-pointer text-white flex items-center justify-center font-bold"
         >
           →
         </motion.div>
-
       </div>
-
     </div>
-
   );
 };
 
